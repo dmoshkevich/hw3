eb35e13c9d7fd4c6a65d364bb98e4fa12d7ac415
Мошкевич Дарья <darya.moshkevich@inside.glowbyte.com>
2021-04-19 13:40:47 +0300

    what year

---
+++ what_year_is_now/result.md
@@ -0,0 +1,12 @@
+
+============================= test session starts =============================
+platform win32 -- Python 3.8.6, pytest-6.2.3, py-1.10.0, pluggy-0.13.1 -- C:\Users\darya.moshkevich\PycharmProjects\pythonProject4\venv\Scripts\python.exe
+cachedir: .pytest_cache
+rootdir: C:\Users\darya.moshkevich\PycharmProjects\pythonProject4
+collecting ... collected 3 items
+
+test_what_is_year_now.py::test_what_is_year_now PASSED                   [ 33%]
+test_what_is_year_now.py::test_what_is_year_now_value_error PASSED       [ 66%]
+test_what_is_year_now.py::test_what_is_year_now_alternative_format PASSED [100%]
+
+============================== 3 passed in 0.04s ==============================
---
+++ what_year_is_now/test_what_is_year_now.py
@@ -0,0 +1,71 @@
+import pytest
+import what_is_year_now
+from unittest.mock import patch, MagicMock
+
+
+@patch('urllib.request.urlopen')
+def test_what_is_year_now(urlopen_mock):
+    test_mock = MagicMock()
+    test_mock.getcode.return_value = 200
+    test_mock.read.return_value = \
+        '{' \
+        '"$id":"1",' \
+        '"currentDateTime":"2021-04-19T09:50Z",' \
+        '"utcOffset":"00:00:00",' \
+        '"isDayLightSavingsTime":false,' \
+        '"dayOfTheWeek":"Monday",' \
+        '"timeZoneName":"UTC",' \
+        '"currentFileTime":132632994190993994,' \
+        '"ordinalDate":"2021-109",' \
+        '"serviceResponse":null' \
+        '}'
+    test_mock.__enter__.return_value = test_mock
+    urlopen_mock.return_value = test_mock
+
+    year = what_is_year_now.what_is_year_now()
+    assert year == 2021
+
+
+@patch('urllib.request.urlopen')
+def test_what_is_year_now_value_error(urlopen_mock):
+    test_mock = MagicMock()
+    test_mock.getcode.return_value = 200
+    test_mock.read.return_value = \
+        '{' \
+        '"$id":"1",' \
+        '"currentDateTime":"djtfjtfhdf",' \
+        '"utcOffset":"00:00:00",' \
+        '"isDayLightSavingsTime":false,' \
+        '"dayOfTheWeek":"Monday",' \
+        '"timeZoneName":"UTC",' \
+        '"currentFileTime":132632994190993994,' \
+        '"ordinalDate":"2021-109",' \
+        '"serviceResponse":null' \
+        '}'
+
+    test_mock.__enter__.return_value = test_mock
+    urlopen_mock.return_value = test_mock
+    with pytest.raises(ValueError):
+        what_is_year_now.what_is_year_now()
+
+
+def test_what_is_year_now_alternative_format(urlopen_mock):
+    test_mock = MagicMock()
+    test_mock.getcode.return_value = 200
+    test_mock.read.return_value = \
+        '{' \
+        '"$id":"1",' \
+        '"currentDateTime":"2021-04-19T09:50Z",' \
+        '"utcOffset":"00:00:00",' \
+        '"isDayLightSavingsTime":false,' \
+        '"dayOfTheWeek":"Monday",' \
+        '"timeZoneName":"UTC",' \
+        '"currentFileTime":132632994190993994,' \
+        '"ordinalDate":"2021-109",' \
+        '"serviceResponse":null' \
+        '}'
+    test_mock.__enter__.return_value = test_mock
+    urlopen_mock.return_value = test_mock
+
+    year = what_is_year_now.what_is_year_now()
+    assert year == 2021
